41ccda1772760b4bd41b79f291a7ebca03ebd8cb
Julia Loboda <yloboda@MacBook-Pro-Julia.local>
2020-03-06 22:02:12 +0200

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError('Not a number');
+  }
+
+  return a + b;
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 module.exports = sum;
