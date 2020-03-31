5bb5c96cb789c0c6b1e779ef53feed553784494f
Julia Loboda <yloboda@MacBook-Pro-Julia.local>
2020-03-31 19:52:27 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,12 +8,32 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+const checkIsReady = () => {
+  return new Promise((resolve, reject) => {
+    resolver = resolve;
+  });
+};
 
+const sendMessage = (msg) => {
+  resolver(msg);
+  delayObj = checkIsReady();
+};
+
+let resolver;
+let delayObj = checkIsReady();
+
+router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await delayObj;
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
-
+  const msg = ctx.request.body.message;
+  if (msg) {
+    sendMessage(msg);
+  }
+  ctx.body = '';
+  return next();
 });
 
 app.use(router.routes());
