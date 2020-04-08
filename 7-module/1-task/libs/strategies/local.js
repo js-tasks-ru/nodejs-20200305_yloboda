805abaa0208c05eed69cac03d88bb1b6b6b56293
Julia Loboda <yloboda@MacBook-Pro-Julia.local>
2020-04-08 21:13:36 +0300

    add solution for 8-module 1-task

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -5,6 +5,9 @@ module.exports = new LocalStrategy(
     {usernameField: 'email', session: false}, async (email, password, done) => {
       User.findOne({'email': email}, async (err, user) => {
         if (!user) done(null, false, 'Нет такого пользователя');
+        if (user.verificationToken) {
+          return done(null, false, 'Подтвердите email');
+        }
         const isValid = await user.checkPassword(password);
         if (!isValid) done(null, false, 'Неверный пароль');
         done(null, user);
--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -1,9 +1,52 @@
 const uuid = require('uuid/v4');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
+const Session = require('../models/Session');
 
 module.exports.register = async (ctx, next) => {
+  const token = uuid();
+
+  const user = new User({
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    password: ctx.request.body.password,
+    verificationToken: token,
+  });
+
+  await user.setPassword(ctx.request.body.password);
+
+  await user.save((err) => {
+    if (err) {
+      ctx.status = 400;
+      ctx.body = {errors: {email: err.errors.email.message}};
+      return next();
+    } else {
+      ctx.status = 200;
+      ctx.body = {status: 'ok'};
+    }
+  });
+
+  await sendMail({
+    template: 'confirmation',
+    locals: {token: token},
+    to: user.email,
+    subject: 'Подтвердите почту',
+  });
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const user = await User.findOneAndUpdate({verificationToken: ctx.request.body.verificationToken});
+
+  if (!user) {
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+    return;
+  }
+
+  user.set('verificationToken', undefined);
+  await user.save();
+
+  const token = uuid();
+
+  ctx.status = 200;
+  ctx.body = {token};
 };
